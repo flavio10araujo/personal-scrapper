641498ae603ed8449ffbefbc4a8cdcb0b3324796
Flavio Moises de Araujo <flavio.dearaujo@ticketmaster.com>
2025-08-05 21:49:56 -0400

    scrapper super c

--- personal_scrapper/canada/quebec/superc/genesis/scrapper.py
+++ personal_scrapper/canada/quebec/superc/genesis/scrapper.py
@@ -36,6 +36,124 @@ def get_main_categories(page):
     ]
     return categories
 
+def get_categories_structure():
+    # Structure for Fruits & Vegetables and Dairy & Eggs with their subcategories
+    return [
+        [
+            # Fruits & Vegetables
+            [
+                # Fruits
+                [
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/apples-pears",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/avocadoes-exotic-fruits",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/bananas-plantains",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/berries-cherries",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/grapes",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/citrus-fruits",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/peaches-nectarines-stone-fruits",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/fruits/watermelons-melons",
+                ],
+                # Vegetables
+                [
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/tomatoes-cucumber",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/potatoes-carrots-celery",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/lettuce-leafy-vegetables",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/peppers-zucchini-eggplant",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/broccoli-cauliflower-cabbage",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/mushrooms",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/peas-beans-corn",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/onions-leeks",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/ginger-garlic-shallots",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/squash-pumpkin",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/asparagus-artichokes",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/root-vegetables",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegetables/exotic-vegetables",
+                ],
+                # Fresh Herbs
+                f"{BASE_URL}/en/aisles/fruits-vegetables/fresh-herbs",
+                # Packaged Salads & Vegetables
+                f"{BASE_URL}/en/aisles/fruits-vegetables/packaged-salads-vegetables",
+                # Fresh-Cut Fruits & Vegetables
+                f"{BASE_URL}/en/aisles/fruits-vegetables/fresh-cut-fruits-vegetables",
+                # Organic Fruits & Vegetables
+                f"{BASE_URL}/en/aisles/fruits-vegetables/organic-fruits-vegetables",
+                # Dried Fruits & Nuts
+                f"{BASE_URL}/en/aisles/fruits-vegetables/dried-fruits-nuts",
+                # Vegan & Vegetarian
+                [
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegan-vegetarian/tofu-tempeh",
+                    f"{BASE_URL}/en/aisles/fruits-vegetables/vegan-vegetarian/vegan-proteins-cheese",
+                ],
+            ]
+        ],
+        [
+            # Dairy & Eggs
+            [
+                # Milk, Cream & Butter
+                [
+                    f"{BASE_URL}/en/aisles/dairy-eggs/milk-cream-butter/2-whole-milk",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/milk-cream-butter/1-skim-milk",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/milk-cream-butter/cream-creamers",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/milk-cream-butter/flavoured-milk",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/milk-cream-butter/lactose-free-non-dairy-milk",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/milk-cream-butter/fermented-milk-kefir",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/milk-cream-butter/butter-margarine",
+                ],
+                # Eggs
+                [
+                    f"{BASE_URL}/en/aisles/dairy-eggs/eggs/whole-eggs",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/eggs/liquid-eggs-egg-whites",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/eggs/organic-eggs",
+                ],
+                # Yogurt
+                [
+                    f"{BASE_URL}/en/aisles/dairy-eggs/yogurt/yogurt-tubs",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/yogurt/multipacks",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/yogurt/greek-yogurts",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/yogurt/drinkable-yogurts",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/yogurt/probiotic-yogurts",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/yogurt/single-serving-yogurts",
+                ],
+                # Packaged Cheese
+                [
+                    f"{BASE_URL}/en/aisles/dairy-eggs/packaged-cheese/fondue",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/packaged-cheese/shredded-cheese",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/packaged-cheese/cream-cheese-spreads",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/packaged-cheese/strings-snacks",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/packaged-cheese/cheese-blocks",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/packaged-cheese/sliced-cheese",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/packaged-cheese/cottage-cheese-ricotta",
+                ],
+                # Deli Cheese
+                [
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/soft-fresh",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/firm-semi-soft",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/mozzarella-feta",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/cheddar",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/parmesan",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/goat-sheep",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/swiss-gouda-jarlsberg",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/blue-cheese-rocquefort",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/deli-cheese/grated-sliced",
+                ],
+                # Sour Cream & Dips
+                [
+                    f"{BASE_URL}/en/aisles/dairy-eggs/sour-cream-dips/sour-cream",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/sour-cream-dips/chilled-dips",
+                ],
+                # Chilled Desserts & Dough
+                [
+                    f"{BASE_URL}/en/aisles/dairy-eggs/chilled-desserts-dough/dessert-cups",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/chilled-desserts-dough/chilled-dough",
+                    f"{BASE_URL}/en/aisles/dairy-eggs/chilled-desserts-dough/whipped-cream",
+                ],
+            ]
+        ],
+        # Other main categories as leaf URLs
+        f"{BASE_URL}/en/aisle/000010",  # Pantry
+        # ... add more as needed ...
+    ]
+
 def scrape_all_categories():
     global seen_skus
     seen_skus = set()
